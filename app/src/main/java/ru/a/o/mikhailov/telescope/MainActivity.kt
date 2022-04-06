8ad5cf4b1826d24cd15ab9323e1a9026fa828d5f
Artem Mikhailov <mega.artemm@bk.ru>
2022-04-06 15:25:27 +0300

    Navigation drawer

--- app/src/main/java/ru/a/o/mikhailov/telescope/MainActivity.kt
+++ app/src/main/java/ru/a/o/mikhailov/telescope/MainActivity.kt
@@ -1,14 +1,38 @@
 package ru.a.o.mikhailov.telescope
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import ru.a.o.mikhailov.telescope.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_Telescope)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+            open.setOnClickListener {
+                drawer.openDrawer(GravityCompat.START)
+            }
+            navView.setNavigationItemSelectedListener {
+                when(it.itemId){
+                    R.id.settings -> {
+                        Toast.makeText(this@MainActivity, "item1", Toast.LENGTH_SHORT).show()
+                        //
+                        //
+                    }
+                    R.id.control -> {
+                        //
+                        //
+                        //
+                    }
+                }
+                true
 
+            }
+        }
     }
 }
