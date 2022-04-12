4595c46fa6827503f5914f7e76dabed78bf0c0b6
Artem Mikhailov <mega.artemm@bk.ru>
2022-04-12 16:46:11 +0300

    Bluetooth adapter

--- app/src/main/java/ru/a/o/mikhailov/telescope/MainActivity.kt
+++ app/src/main/java/ru/a/o/mikhailov/telescope/MainActivity.kt
@@ -1,18 +1,24 @@
 package ru.a.o.mikhailov.telescope
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import ru.a.o.mikhailov.telescope.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         setTheme(R.style.Theme_Telescope)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
         supportFragmentManager
             .beginTransaction()
@@ -41,4 +47,5 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
 }
--- app/src/main/java/ru/a/o/mikhailov/telescope/SettingsFragment.kt
+++ app/src/main/java/ru/a/o/mikhailov/telescope/SettingsFragment.kt
@@ -1,30 +1,52 @@
 package ru.a.o.mikhailov.telescope
 
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-
+import ru.a.o.mikhailov.telescope.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
 
+    private var btAdapter: BluetoothAdapter? = null
+    private lateinit var binding: FragmentSettingsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        binding = FragmentSettingsBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        init()
     }
 
     companion object {
         @JvmStatic
         fun newInstance() = SettingsFragment()
+    }
+
+    private fun init(){
+        val btManager = requireActivity().getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        btAdapter = btManager.adapter
+        getPairedDevices()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getPairedDevices(){
+        val pairedDevices: Set<BluetoothDevice>? = btAdapter?.bondedDevices
+        pairedDevices?.forEach{
+            Log.d("mylog", "Name: ${it.name}")
+        }
     }
 }
